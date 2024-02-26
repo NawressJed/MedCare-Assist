4fb0b50b5d98b060bffecdf7cc5ef992f809b044
NawressJed <nawress.jed@gmail.com>
2024-02-26 19:28:46 +0100

    first commit

---
+++ src/main/java/com/example/medcareassist/MedCareAssistApplication.java
@@ -0,0 +1,13 @@
+package com.example.medcareassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCareAssistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedCareAssistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medcareassist/MedCareAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medcareassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCareAssistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
