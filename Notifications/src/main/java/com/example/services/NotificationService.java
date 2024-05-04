6501c06de75d95a977db8ef3cab2136b204f0971
nawressJed <nawress.jed@gmail.com>
2024-05-04 14:18:07 +0100

    Added Notification Module with Appointment Notifications and enhanced entities

--- Notifications/src/main/java/com/example/services/NotificationService.java
+++ Notifications/src/main/java/com/example/services/NotificationService.java
@@ -1,12 +1,7 @@
 package com.example.services;
 
 
-import com.example.dto.response.AppointmentNotificationResponse;
 import com.example.entities.Appointment;
-import com.example.entities.Doctor;
-import com.example.entities.Patient;
-
-import java.util.UUID;
 
 public interface NotificationService {
     void sendAppointmentRequestNotificationToDoctor(Appointment appointment);
